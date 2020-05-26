b2cb3074e862a89e03fb7ffd62cde79b7375812c
omjadas <omja.das@gmail.com>
2020-05-26 21:53:35 +1000

    feat: implement commands

--- src/main.adb
+++ src/main.adb
@@ -15,25 +15,98 @@ procedure Main is
    PIN2  : PIN.PIN := PIN.From_String("1234");
    package Lines is new MyString(Max_MyString_Length => 2048);
    S  : Lines.MyString;
-   LOCKED : Boolean := true;
-   MASTERPIN : PIN.PIN;
+   Locked : Boolean := true;
+   Masterpin : PIN.PIN;
 begin
 
    if MyCommandLine.Argument_Count /= 1 then
       return;
    end if;
 
-   MASTERPIN := Pin.From_String(MyCommandLine.Argument(1));
+   Masterpin := PIN.From_String(MyCommandLine.Argument(1));
    PasswordDatabase.Init(DB);
 
    loop 
-      if LOCKED = true then
+      if Locked = true then
          Put("locked>   ");
       else
          Put("unlocked> ");
       end if;
       Lines.Get_Line(S);
-      exit when Lines.To_String(S) = "";
+      declare
+         T : MyStringTokeniser.TokenArray(1..5) := (others => (Start => 1, Length => 0));
+         NumTokens : Natural;
+      begin
+         MyStringTokeniser.Tokenise(Lines.To_String(S),T,NumTokens);
+         declare
+            Command : String := Lines.To_String(Lines.Substring(S,T(1).Start,T(1).Start+T(1).Length-1));
+         begin
+            if Command = "get" then
+               if NumTokens > 2 then
+                  return;
+               elsif NumTokens = 2 then
+                  declare
+                     U : PasswordDatabase.URL := PasswordDatabase.From_String(Lines.To_String(Lines.Substring(S,T(2).Start,T(2).Start+T(2).Length-1)));
+                  begin
+                     if not Locked then
+                        Put_Line(PasswordDatabase.To_String(PasswordDatabase.Get(DB,U)));
+                     end if;
+                  end;
+               end if;
+            elsif Command = "rem" then
+               if NumTokens > 2 then
+                  return;
+               elsif NumTokens = 2 then
+                  declare
+                     U : PasswordDatabase.URL := PasswordDatabase.From_String(Lines.To_String(Lines.Substring(S,T(2).Start,T(2).Start+T(2).Length-1)));
+                  begin
+                     if not Locked then
+                        PasswordDatabase.Remove(DB,U);
+                     end if;
+                  end;
+               end if;
+            elsif Command = "put" then
+               if NumTokens > 3 then
+                  return;
+               elsif NumTokens = 3 then
+                  declare
+                     U : PasswordDatabase.URL := PasswordDatabase.From_String(Lines.To_String(Lines.Substring(S,T(2).Start,T(2).Start+T(2).Length-1)));
+                     P : PasswordDatabase.Password := PasswordDatabase.From_String(Lines.To_String(Lines.Substring(S,T(3).Start,T(3).Start+T(3).Length-1)));
+                  begin
+                     if not Locked then
+                        PasswordDatabase.Put(DB,U,P);
+                     end if;
+                  end;
+               end if;
+            elsif Command = "unlock" then
+               if NumTokens > 2 then
+                  return;
+               elsif NumTokens = 2 then
+                  declare
+                     Number : PIN.PIN := PIN.From_String(Lines.To_String(Lines.Substring(S,T(2).Start,T(2).Start+T(2).Length-1)));
+                  begin
+                     if Locked and PIN."="(Masterpin,Number) then
+                        Locked := false;
+                     end if;
+                  end;
+               end if;
+            elsif Command = "lock" then
+               if NumTokens > 2 then
+                  return;
+               elsif NumTokens = 2 then
+                  declare
+                     Number : PIN.PIN := PIN.From_String(Lines.To_String(Lines.Substring(S,T(2).Start,T(2).Start+T(2).Length-1)));
+                  begin
+                     if not Locked and PIN."="(Masterpin,Number) then
+                        Locked := true;
+                     end if;
+                  end;
+               end if;
+            else
+               return;
+            end if;
+         end;
+      end;
    end loop;
 
    Put_Line("Adding an entry to the database");
